ed389185ada55ee93497efae62ab5a11c161e02f
kennCK <kennettecanales@gmail.com>
2019-06-04 10:28:04 +0800

    Fixed ratings

--- rating/DirectRatings.vue
+++ rating/DirectRatings.vue
@@ -1,6 +1,6 @@
 <template>
-  <span>
-    <i v-bind:class="{'far': stars === 0 || i > stars || status === false, 'fas text-warning': i <= stars}" class="fa-star" v-for="i in 5"></i> <label v-if="status !== false">({{avg.toFixed(1)}})</label>
+  <span v-if="ratings !== null">
+    <i v-bind:class="{'far': ratings.stars === 0 || i > ratings.stars, 'fas text-warning': i <= ratings.stars}" class="fa-star" v-for="i in 5"></i> <label v-if="parseInt(ratings.size) > 10">({{ratings.size}})</label>
   </span>
 </template>
 <script>
@@ -10,52 +10,17 @@ import CONFIG from '../../../../config.js'
 import axios from 'axios'
 export default {
   mounted(){
-    this.retrieve()
   },
   data(){
     return {
       user: AUTH.user,
-      config: CONFIG,
-      total: 0,
-      avg: 0,
-      data: null,
-      stars: 0,
-      status: true
+      config: CONFIG
     }
   },
-  props: ['payload', 'payloadValue'],
+  props: ['ratings'],
   methods: {
     redirect(parameter){
       ROUTER.push(parameter)
-    },
-    retrieve(){
-      let parameter = {
-        account_id: this.user.userID,
-        condition: [{
-          value: this.payload,
-          column: 'payload',
-          clause: '='
-        }, {
-          value: this.payloadValue,
-          column: 'payload_value',
-          clause: '='
-        }]
-      }
-      this.APIRequest('ratings/retrieve', parameter).then(response => {
-        if(response.data !== null){
-          this.data = response.data
-          this.total = response.total
-          this.avg = parseFloat(response.avg)
-          this.stars = response.stars
-          this.status = response.status
-        }else{
-          this.data = null
-          this.total = 0
-          this.avg = 0
-          this.stars = 0
-          this.status = true
-        }
-      })
     }
   }
 }
