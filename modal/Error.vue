f90788d9d375d29a01a2282bc86bc59966268afb
kennCK <kennettecanales@gmail.com>
2020-02-22 16:27:49 +0800

    update

---
+++ modal/Error.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="modal fade" id="connectionError" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title text-danger" id="exampleModalLabel">Error Request</h5>
+          <button type="button" class="close" @click="hideModal()" aria-label="Close">
+            <span aria-hidden="true" class="text-primary">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <label class="text-danger">There's a network interruption. Kindly check your network and get back!</label>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" @click="hideModal()">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+@import "~assets/style/colors.scss";
+</style>
+<script>
+import ROUTER from 'src/router'
+import AUTH from 'src/services/auth'
+import CONFIG from 'src/config.js'
+import COMMON from 'src/common.js'
+export default {
+  mounted(){
+  },
+  data(){
+    return {
+      user: AUTH.user,
+      auth: AUTH,
+      config: CONFIG
+    }
+  },
+  methods: {
+    show(){
+      $('#connectionError').modal('show')
+    },
+    hideModal(){
+      $('#connectionError').modal('hide')
+    }
+  }
+}
+</script>
