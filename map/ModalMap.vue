d2b76568366fcd58e5233ad1d945c5b2b20d7071
kennCK <kennettecanales@gmail.com>
2020-03-28 15:16:31 +0800

    update

--- map/ModalMap.vue
+++ map/ModalMap.vue
@@ -10,21 +10,12 @@
     >
       <div class="modal-dialog modal-full" role="document">
         <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Modal</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">×</span>
-            </button>
-          </div>
           <div class="modal-body" id="result">
             <div class="row">
               <div class="col-sm-12 col-lg-12">
                 <div id="map" ref="map"></div>
               </div>
             </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" @click="hideModal">close</button>
           </div>
         </div>
       </div>
@@ -42,7 +33,6 @@ export default {
   components: {
     googlemap: require('./GoogleMap')
   },
-  props: ['data', 'property'],
   methods: {
     showModal(){
       $('#mapmodal').modal('show')
